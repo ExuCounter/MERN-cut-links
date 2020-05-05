a11e4ec452a5ede7296690eb30be3f73a026b3c8
Volodymyr Potiichuk <exucounter@gmail.com>
2020-05-05 14:59:42 +0300

    connect mongoDB

--- app.js
+++ app.js
@@ -6,4 +6,19 @@ const config = require('config');
 
 const PORT = config.get('port') || 3000;
 
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    }
+}
+
+start();
+
 app.listen(PORT, ()=>{console.log(`App listen port ${PORT}`)})
