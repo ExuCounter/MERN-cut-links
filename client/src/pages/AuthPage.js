e42ca67f4e39d2951ad1c26c55251c093b8f3b5e
Volodymyr Potiichuk <exucounter@gmail.com>
2020-05-08 14:12:47 +0300

    Handler on auth form

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const AuthPage = () => {
+
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
+    /* С помощью деструктуризации мы ищем элемент который изменился,
+    и заменяем его значение на новое */
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
     return(
         <div className="row">
             <div className="col s6 offset-s3">
@@ -10,8 +23,21 @@ export const AuthPage = () => {
                     <span className="card-title">Authorization</span>
                  </div>
                  <div className='card-action'>
-                     <input id='email' placeholder="Email" type="text" class="validate"/>
-                     <input id='password' placeholder="Password" type='password' style={{marginBottom: '40px', display: 'block'}}/>
+                     <input 
+                        id='email'
+                        name='email'
+                        placeholder="Email"
+                        type="text"
+                        class="validate"
+                        onChange='changeHandler'
+                         />
+                     <input 
+                        id='password'
+                        name='password'
+                        placeholder="Password"
+                        type='password'
+                        style={{marginBottom: '40px', display: 'block'}}
+                        />
                      <button className="btn yellow darken-4" style={{marginRight: '10px'}}>Sign In</button>
                      <button className="btn grey lighten-1 ml-2 black-text">Sign Up</button>
                  </div>
